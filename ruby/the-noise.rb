f58a6c6048662db5c6333ca0c5682d59d3941f24
Leon Martin <13058234+malvabombom@users.noreply.github.com>
2022-11-08 13:48:37 -0600

    Create the-noise.rb

---
+++ ruby/the-noise.rb
@@ -0,0 +1,53 @@
+
+
+live_loop :hight do
+  use_synth :prophet
+  play 38
+  sleep 0.25
+  play 50
+  sleep 0.25
+  play 62
+  sleep 0.25
+  play 45
+  sleep 0.25
+  play 35
+  sleep 0.25
+  play 43
+  play 38
+  sleep 0.25
+  play 50
+  sleep 0.25
+  play 62
+  sleep 0.25
+  play 45
+  sleep 0.25
+  play 35
+  sleep 0.25
+  play 43
+end
+
+live_loop :chords do
+  use_synth :saw
+  play 38
+  sleep 0.25
+  play 50
+  sleep 0.25
+  play 62
+  sleep 0.25
+  play 32
+  sleep 0.25
+  play 45
+  sleep 0.25
+  play 64
+  play 38
+  sleep 0.25
+  play 50
+  sleep 0.25
+  play 62
+  sleep 0.25
+  play 32
+  sleep 0.25
+  play 45
+  sleep 0.25
+  play 64
+end
