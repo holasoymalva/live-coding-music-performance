769a93af4b225ceff0fa074b7d3c1d9e3d0ad3f0
Leon Martin <13058234+malvabombom@users.noreply.github.com>
2022-11-08 14:21:59 -0600

    Update a-robot-thinking.rb

--- ruby/a-robot-thinking.rb
+++ ruby/a-robot-thinking.rb
@@ -51,3 +51,54 @@ live_loop :chords do
   play 55, release: 0.2
   sleep 0.25
 end
+
+
+live_loop :robotDream do
+  use_synth :tb303
+  play 38
+  sleep 0.25
+  use_synth :dsaw
+  play 50
+  sleep 0.25
+  use_synth :prophet
+  play 57
+  sleep 0.25
+  use_synth :saw
+  play 38
+  sleep 0.25
+  play 50
+  sleep 0.25
+  play 20
+  sleep 0.25
+  use_synth :prophet
+  play 57, release: 0.2
+  sleep 0.25
+  play 57
+  sleep 0.25
+  play 60, release: 0.2
+  sleep 0.25
+end
+
+live_loop :hight do
+  use_synth :prophet
+  play 45
+  sleep 0.25
+  play 50
+  sleep 0.25
+  play 38
+  sleep 0.25
+  play 45
+  sleep 0.25
+end
+
+live_loop :chords do
+  use_synth :saw
+  play 45
+  sleep 0.25
+  play 50
+  sleep 0.25
+  play 38
+  sleep 0.25
+  play 45
+  sleep 0.25
+end
